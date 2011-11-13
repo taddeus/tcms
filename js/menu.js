acbe79bd764fffb66e2047dec36766804d873039
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-11-14 00:59:50 +0100

    Added basic menu AJAX functionality.

---
+++ js/menu.js
@@ -0,0 +1,34 @@
+(function($) {
+	var REFRESH_INTERVAL = 100,
+		url = location.hash.replace(/^#/, ''),
+		load_content = function(url) {
+			// Menu active class
+			$('#menu a[href=#' + url + ']').addClass('active')
+				.siblings('.active').removeClass('active');
+			
+			// AJAX call for content
+			$('#content').load(url);
+		},
+		refresh_url = function(new_url) {
+			if( new_url != url ) {
+				url = new_url;
+				load_content(url);
+			}
+		},
+		interval,
+		reset_interval = function() {
+			clearInterval(interval);
+			interval = setInterval(function() {
+				refresh_url(location.hash.replace(/^#/, ''));
+			}, REFRESH_INTERVAL);
+		};
+	
+	// Default selected link
+	if( url == '' )
+		url = location.hash = 'pages';
+	
+	$('a[href^=#]').live('click', function() {
+		reset_interval();
+		refresh_url($(this).attr('href').substring(1));
+	});
+})(jQuery);
